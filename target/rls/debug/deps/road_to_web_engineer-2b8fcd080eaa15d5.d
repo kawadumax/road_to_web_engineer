ec8503bcbb23d3b8c66ae970dde65209f7ce4532
kawadumax <kazmax7@gmail.com>
2020-05-04 23:44:36 +0900

    remove target folder

--- target/rls/debug/deps/road_to_web_engineer-2b8fcd080eaa15d5.d
+++
@@ -1,5 +0,0 @@
-c:\Users\kazuma\Documents\rust_projects\road_to_web_engineer\target\rls\debug\deps\road_to_web_engineer-2b8fcd080eaa15d5.rmeta: src\main.rs
-
-c:\Users\kazuma\Documents\rust_projects\road_to_web_engineer\target\rls\debug\deps\road_to_web_engineer-2b8fcd080eaa15d5.d: src\main.rs
-
-src\main.rs:
