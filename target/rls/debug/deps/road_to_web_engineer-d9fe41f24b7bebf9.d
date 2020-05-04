bc3e5e56c1233bfb6f6a31e6434479254126c990
kawadumax <kazmax7@gmail.com>
2020-05-04 23:28:55 +0900

    add actions

---
+++ target/rls/debug/deps/road_to_web_engineer-d9fe41f24b7bebf9.d
@@ -0,0 +1,5 @@
+c:\Users\kazuma\Documents\rust_projects\road_to_web_engineer\target\rls\debug\deps\road_to_web_engineer-d9fe41f24b7bebf9.rmeta: src\main.rs
+
+c:\Users\kazuma\Documents\rust_projects\road_to_web_engineer\target\rls\debug\deps\road_to_web_engineer-d9fe41f24b7bebf9.d: src\main.rs
+
+src\main.rs:
